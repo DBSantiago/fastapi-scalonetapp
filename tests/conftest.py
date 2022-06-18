cfd896f3a257e06df5f2830431b7d08399aa8709
DBSantiago <deblasissantiago@gmail.com>
2022-06-18 04:33:55 -0300

    ADD: Unit Tests for Usuarios

---
+++ tests/conftest.py
@@ -0,0 +1,84 @@
+from datetime import datetime
+
+import pytest
+from decouple import config
+from jose import jwt
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+from fastapi.testclient import TestClient
+
+from project import app
+from project.database import get_db, Base
+from project.models import Usuario
+from project.oauth2 import SECRET_KEY, ALGORITHM
+from project.schemas import Token
+from project.utils import hash_password
+
+TEST_DB_HOST = config("TEST_DB_HOST")
+TEST_DB_USER = config("TEST_DB_USER")
+TEST_DB_PASSWORD = config("TEST_DB_PASSWORD")
+TEST_DB_PORT = config("TEST_DB_PORT")
+# SQLALCHEMY_DATABASE_URL = config("DATABASE_URL_2")
+
+SQLALCHEMY_TEST_DATABASE_URL = f"postgresql://{TEST_DB_USER}:{TEST_DB_PASSWORD}@{TEST_DB_HOST}:{TEST_DB_PORT}/test_scalonetapp"
+
+engine = create_engine(SQLALCHEMY_TEST_DATABASE_URL)
+
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+@pytest.fixture()
+def session():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture()
+def client(session):
+    def override_get_db():
+        """
+        Creates a SQLAlchemy SessionLocal dependency that will be used in a single request.
+        It is closed once the request is finished.
+        """
+        try:
+            yield session
+        finally:
+            session.close()
+
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
+
+
+@pytest.fixture
+def usuario_test(session):
+    new_usuario = Usuario(email="test@email.com", password=hash_password("password123"))
+
+    session.add(new_usuario)
+    session.commit()
+    session.refresh(new_usuario)
+
+    return {
+        "id": new_usuario.id,
+        "email": new_usuario.email,
+        "password": "password123"
+    }
+
+
+@pytest.fixture
+def usuario_login(client, usuario_test):
+    response = client.post("/api/v1/auth/login", data={"username": usuario_test.get("email"),
+                                                       "password": usuario_test.get("password")})
+
+    login_res = Token(**response.json())
+    payload = jwt.decode(login_res.access_token, SECRET_KEY, algorithms=[ALGORITHM])
+    usuario_id = payload.get("usuario_id")
+
+    assert usuario_id == usuario_test.get("id")
+
+    return login_res
---
+++ tests/test_usuario.py
@@ -0,0 +1,107 @@
+import pytest
+from decouple import config
+from jose import jwt
+
+from project.models import Usuario
+from project.oauth2 import SECRET_KEY, ALGORITHM
+
+from project.schemas import UsuarioResponseModel, Token
+from project.utils import hash_password
+
+ADMIN_EMAIL = config("ADMIN_EMAIL")
+ADMIN_PASSWORD = hash_password(config("ADMIN_PASSWORD"))
+
+USUARIOS_URL = "/api/v1/usuarios"
+AUTH_URL = "/api/v1/auth/login"
+
+
+class TestUsuariosClass:
+
+    def test_create_usuario(self, client):
+        response = client.post(f"{USUARIOS_URL}/", json={"email": "test2@email.com",
+                                                         "password": "password"})
+        new_usuario = UsuarioResponseModel(**response.json())
+        assert response.status_code == 200
+        assert new_usuario.email == "test2@email.com"
+
+    def test_authenticate_usuario(self, client, usuario_test):
+        response = client.post(f"{AUTH_URL}", data={"username": usuario_test.get("email"),
+                                                    "password": usuario_test.get("password")})
+
+        login_res = Token(**response.json())
+        payload = jwt.decode(login_res.access_token, SECRET_KEY, algorithms=[ALGORITHM])
+        usuario_id = payload.get("usuario_id")
+
+        assert response.status_code == 200
+        assert login_res.token_type == "Bearer"
+        assert usuario_id == usuario_test.get("id")
+
+    @pytest.mark.parametrize("email, password, status_code", [
+        ("wrong-email@email.com", "password123", 403),
+        ("test@email.com", "wrong-password", 403),
+        ("wrongemail@email.com", "wrong-password", 403),
+        (None, "password123", 422),
+        ("test@email.com", None, 422)
+    ])
+    def test_authenticate_usuario_failed(self, client, usuario_test, email, password, status_code):
+        response = client.post(f"{AUTH_URL}", data={"username": email,
+                                                    "password": password})
+        assert response.status_code == status_code
+
+    def test_get_usuario(self, client, usuario_test):
+        response = client.get(f"{USUARIOS_URL}/{usuario_test.get('id')}")
+        assert response.status_code == 200
+        assert response.json().get("id") == usuario_test.get("id")
+        assert response.json().get("email") == usuario_test.get("email")
+
+    def test_get_usuario_error404(self, client):
+        response = client.get(f"{USUARIOS_URL}/99999")
+        assert response.status_code == 404
+        assert response.json().get("detail") == "Usuario inexistente."
+
+    @pytest.mark.parametrize("updated_email, updated_password", [
+        ("updated-email@email.com", "password123"),
+        ("test@email.com", "123password"),
+        ("updated-email@email.com", "123password"),
+    ])
+    def test_update_usuario(self, client, usuario_test, usuario_login,
+                            updated_email, updated_password):
+        response = client.put(f"{USUARIOS_URL}/{usuario_test.get('id')}",
+                              json={"email": updated_email, "password": updated_password},
+                              headers={
+                                  'Authorization': f'{usuario_login.token_type} {usuario_login.access_token}'})
+        assert response.status_code == 200
+        assert response.json().get("email") == updated_email
+        assert response.json().get("id") == usuario_test.get("id")
+
+    @pytest.mark.parametrize("email, password, status_code", [
+        ("new-email@email.com", "password123", 200),
+        ("test@email.com", "new-password", 200),
+        ("new-email@email.com", "new-password", 200),
+        (None, "password123", 422),
+        ("test@email.com", None, 422)
+    ])
+    def test_update_usuario_error(self, client, usuario_test, usuario_login, email, password, status_code):
+        response = client.put(f"{USUARIOS_URL}/{usuario_test.get('id')}",
+                              json={"email": email, "password": password},
+                              headers={
+                                  'Authorization': f'{usuario_login.token_type} {usuario_login.access_token}'}
+                              )
+        assert response.status_code == status_code
+
+    def test_delete_usuario(self, client, usuario_test, usuario_login):
+        response = client.delete(f"{USUARIOS_URL}/{usuario_test.get('id')}",
+                                 headers={
+                                     'Authorization': f'{usuario_login.token_type} {usuario_login.access_token}'}
+                                 )
+
+        assert response.status_code == 200
+        assert response.json() == {"OK": f"Usuario con id: {usuario_test.get('id')} eliminado exitosamente"}
+
+    def test_delete_usuario_error404(self, client, usuario_test, usuario_login):
+        response = client.delete(f"{USUARIOS_URL}/99999",
+                                 headers={
+                                     'Authorization': f'{usuario_login.token_type} {usuario_login.access_token}'}
+                                 )
+
+        assert response.status_code == 404
